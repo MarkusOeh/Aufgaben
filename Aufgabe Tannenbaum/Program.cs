70920027dc349b3c5dbf85dc27d45c91cebf7425
MarkusOeh <168172009+MarkusOeh@users.noreply.github.com>
2024-07-03 22:01:33 +0200

    Add files via upload
    Aufgabe vom 03. Juli 24

---
+++ Aufgabe Tannenbaum/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Aufgabe_Tannenbaum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BaumBauer v.1.0");
+            int hashNumber = 1;
+            int whitespaceAmount = height - 2;
+            for (int i = 0; i < height; i++) // Eine Zeile
+            {
+                // Whitespace vor dem #
+                for (int j = 0; j < whitespaceAmount; j++)
+                {
+                    Console.Write(" ");
+                }
+                // hashtags => #
+                for (int j = 0; j < hashNumber; j++)
+                {
+                    Console.Write("#");
+                }
+                /* auch mit if möglich
+                 if (height == i - 1) { }
+                 Nach jeder Zeile
+                hashNumber += 2;
+                */
+                whitespaceAmount--;
+                Console.WriteLine();
+            }
+            // spezialfall
+            whitespaceAmount = height - 2;
+            // Whitespace vor dem #
+            for (int j = 0; j < whitespaceAmount; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("#");
+        }
+    }
+}
