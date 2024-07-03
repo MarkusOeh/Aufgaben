e4858b236cdec28d3e80384612c589c963e1afc5
MarkusOeh <168172009+MarkusOeh@users.noreply.github.com>
2024-07-03 22:19:23 +0200

    Add files via upload
    Hausaufgabe vom 03. Juli 24

---
+++ Aufgabe OnlyEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Aufgabe_OnlyEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ---------------------------------- AUFGABE ----------------------------------
+            // Gebe eine neue Liste zurück, welche nur alle geraden Zahlen beinhaltet.
+            // Input: [1,2,3,4,5,6]
+            // Output: [2,4,6]
+            // Formel "if (i % 2 == 0)" prüft ob Zahlen durch 2 teilbar sind
+
+            List<string> ausgabeListe = new();
+            List<int> inputNumbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+            Console.WriteLine($"WIR HABEN DIE ZAHLEN {inputNumbers[0]}, {inputNumbers[1]}, {inputNumbers[2]}, {inputNumbers[3]}, {inputNumbers[4]}, {inputNumbers[5]}\nPRÜFE AUF GERADE ZAHLEN\n");
+
+            for (int i = 0; i < inputNumbers.Count; i++)
+            {
+                if (inputNumbers[i] % 2 == 0) 
+                {
+                    Console.WriteLine($"ZAHL {inputNumbers[i]} IST EINE GERADE ZAHL");
+                }
+            }
+
+        }
+    }
+}
