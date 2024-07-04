ad31b468ab2415ee16553d2ffa3d67f47072d79e
MarkusOeh <168172009+MarkusOeh@users.noreply.github.com>
2024-07-04 12:20:48 +0200

    Aufgabe vom 04. Juli 24

---
+++ Aufgabe PrintBackwards/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Aufgabe_PrintBackwards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ---------------------- AUFGABE ----------------------
+            // Drucke den gegeben Text rückwärts, wobei jedes Zeichen mit einem Leerzeichen getrennt sein soll.
+            // Input: "Hamster"
+            // Output: "r e t s m a H"
+
+            Console.WriteLine("GIB EIN WORT EIN");
+            string inputWord = Console.ReadLine();
+            List<char> buchstabenListe = new List<char>();
+
+            Console.WriteLine();
+
+            foreach (char buchstabe in inputWord)
+            {
+                buchstabenListe.Add(buchstabe);
+            }
+
+            // Ausgabe der Liste
+            Console.WriteLine("BESTEHT AUS:");
+            foreach (char buchstabe in buchstabenListe)
+            {
+                Console.WriteLine(buchstabe);
+            }
+            
+            Console.WriteLine();
+
+            // Ausgabe der Liste in umgekehrter Reihenfolge
+            Console.WriteLine("DIE BUCHSTABEN UMGEKEHRT SIND:");
+            for (int i = buchstabenListe.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(buchstabenListe[i]);
+            }
+        }
+    }
+}
