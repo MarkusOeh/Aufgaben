8e1e8f098d45dfd27b4d9a921220ca9b4ed6cd28
MarkusOeh <168172009+MarkusOeh@users.noreply.github.com>
2024-07-04 12:21:58 +0200

    Aufgabe vom 04. Juli 24

---
+++ Aufgabe Wörter Zählen/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aufgabe_Wörter_Zählen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ---------------------- AUFGABE ----------------------
+            // Eine Funktion, welche die Anzahl von Worten in einer beliebigen Zeichenkette zählt.
+            // Ein Wort besteht aus mehreren zusammenhängenden Buchstaben
+            // und werden mit Leerzeichen voneinander getrennt.
+            // Input: "Hier könnte Ihre Werbung stehen!"
+            // Output: 5
+
+
+        }
+    }
+}
