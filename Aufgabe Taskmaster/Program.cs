a3630ec9a803ab45ace05566b98e401fcf8eebcb
MarkusOeh <168172009+MarkusOeh@users.noreply.github.com>
2024-07-09 22:15:11 +0200

    Hausaufgabe vom 09. Juli 24

---
+++ Aufgabe Taskmaster/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Aufgabe_Taskmaster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ich erstelle für den Anfang die folgenden 3 Objekte (task1, task2, task3) für unsere Klasse "Task.cs" :
+            Task task1 = new Task("Dokumentation schreiben", "Schreibe die Projektdokumentation bis zum Ende der Woche", new DateTime(2024, 7, 7), false);
+            Task task2 = new Task("Code-Review durchführen", "Überprüfe den Code des neuen Features", new DateTime(2024, 7, 5), false);
+            Task task3 = new Task("Einkaufen gehen", "Lebensmittel kaufen um etwas leckeres zu kochen :)", new DateTime(2024, 7, 9), false);
+
+            do // Hier startet unsere alte ToDo App die für unsere neue Aufgabenstellung angepasst wurde
+            {
+                Console.Clear();
+                Console.WriteLine("Wir haben bereits 3 Tasks für dich erstellt. Was möchtest du tun?");
+                Console.WriteLine("\t1. Tasks Beschreibung abrufen\n\t2. Die erste Aufgabe als erledigt markieren\n\t3. Fälligkeitsdatum der zweiten Aufgabe verlängern");
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        // Tasks mit ihren Beschreibungen ausgeben. Die Methode "GetTaskInfo()" wird an Objekt 1,2 und 3 ausgeführt
+                        Console.WriteLine(task1.GetTaskInfo());
+                        Console.WriteLine(task2.GetTaskInfo());
+                        Console.WriteLine(task3.GetTaskInfo());
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        // Die erste Aufgabe als erledigt markieren. Die Methode "CompleteTask()" setzt bool auf true bei unserem Objekt (später: "...ERLEDIGT: true")
+                        task1.CompleteTask();
+                        Console.WriteLine(task1.GetTaskInfo());
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        // Fälligkeitsdatum der zweiten Aufgabe verlängern. Methode "ExtendDueDate" wird ausgeführt mit neuem Wert innerhalb für Objekt task2
+                        task2.ExtendDueDate(new DateTime(2024, 7, 10));
+                        Console.WriteLine(task2.GetTaskInfo());
+                        Console.ReadKey();
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Aufgabe Taskmaster/Task.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_Taskmaster
+{
+    public class Task
+    {
+        // Wir definieren die 4 Eigenschaften/Properties die unsere künftigen Objekte haben können
+        internal string Title { get; set; }
+        internal string Description { get; set; }
+        internal DateTime DueDate { get; set; }
+        internal bool IsCompleted { get; set; }
+
+        // Die folgenden Zeilen werden "Konstruktor" genannt:
+        public Task(string title, string description, DateTime dueDate, bool isCompleted)
+        {
+            Title = title;
+            Description = description;
+            DueDate = dueDate;
+            IsCompleted = isCompleted;
+        }
+
+        // Wenn wir die Methode GetTaskInfo() an unserem Objekt aufrufen erscheinen die Eigenschaften/Properties unseres Objekts in der Konsole
+        public string GetTaskInfo()
+        {
+            return $"\nTITEL: {Title}, \nBESCHREIBUNG; {Description}, \nFÄLLIG BIS: {DueDate}, \nERLEDIGT: {IsCompleted}\n";
+        }
+
+        // Markierung der Aufgabe als erledigt. Methode CompleteTask() ändert das bool auf true wenn sie ausgeführt wird (egal welches Objekt)
+        public void CompleteTask()
+        {
+            IsCompleted = true ;
+        }
+
+        // Verlängerung des Fälligkeitsdatums. Das grüne DateTime kommt aus C# und der Umgang damit muss auswendig gelernt werden.
+        public void ExtendDueDate(DateTime newDueDate)
+        {
+            DueDate = newDueDate;
+        }
+    }
+}
