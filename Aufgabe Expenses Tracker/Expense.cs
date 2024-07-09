c9d6a1d11538920bb8838261eb4a82969f24a1ff
MarkusOeh <168172009+MarkusOeh@users.noreply.github.com>
2024-07-09 08:40:07 +0200

    Copy Paste von Tobias

---
+++ Aufgabe Expenses Tracker/Expense.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_Expenses_Tracker
+{
+    internal class Expense
+    {
+        // Properties
+        internal string Name { get; set; }
+        internal decimal Price { get; set; }
+        internal string Category { get; set; }
+        internal DateTime CreatedAt { get; set; }
+
+        public Expense(string name, decimal price, string category)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
