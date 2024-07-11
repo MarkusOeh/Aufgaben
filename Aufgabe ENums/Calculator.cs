1686deadf7d55b0b8bf850ead35dc55ccf983116
MarkusOeh <168172009+MarkusOeh@users.noreply.github.com>
2024-07-11 21:04:51 +0200

    Hausaufgabe vom 11. Juli 24

---
+++ Aufgabe ENums/Calculator.cs
@@ -0,0 +1,81 @@
+﻿namespace Calculator_mit_ENums
+{
+    public static class Calculator
+    {
+        public static double Count { get; set; }
+
+        public static double GetNumberInput(string inputPrompt)
+        {
+            Console.Write(inputPrompt);
+            return double.Parse(Console.ReadLine());
+        }
+
+        public static double Calculate(double numberInput1, double numberInput2, Operator op)
+        {
+            double result = 0;
+            Count++;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            switch (op)
+            {
+                case Operator.Add:
+                    result = numberInput1 + numberInput2;
+                    Console.WriteLine($"\nDas Ergebnis von {numberInput1} + {numberInput2} = {Math.Round(result, 2)}\n");
+                    break;
+                case Operator.Subtract:
+                    result = numberInput1 - numberInput2;
+                    Console.WriteLine($"\nDas Ergebnis von {numberInput1} - {numberInput2} = {Math.Round(result, 2)}\n");
+                    break;
+                case Operator.Multiply:
+                    result = numberInput1 * numberInput2;
+                    Console.WriteLine($"\nDas Ergebnis von {numberInput1} * {numberInput2} = {Math.Round(result, 2)}\n");
+                    break;
+                case Operator.Divide:
+                    result = numberInput1 / numberInput2;
+                    Console.WriteLine($"\nDas Ergebnis von {numberInput1} / {numberInput2} = {Math.Round(result, 2)}\n");
+                    break;
+                case Operator.Triangle:
+                    result = (numberInput1 * numberInput2) / 2;
+                    Console.WriteLine($"\n({numberInput1} * {numberInput2}) : 2 = {Math.Round(result, 2)} cm2 ist der Flächeninhalt des Dreiecks\n");
+                    result = numberInput1 + numberInput1 + numberInput1;
+                    Console.WriteLine($"Der Umfang des (gleichseitigen) Dreiecks beträgt: {Math.Round(result, 2)}cm\n");
+
+
+                    //double kathete = Math.Sqrt((2*result) / numberInput2);
+                    //double umfang = kathete + kathete + numberInput1;
+                    //result = umfang;
+                    //Console.WriteLine($"Umfang bei einem gleichschenkligen rechtwinkligen Dreieck: {Math.Round(result, 2)}\n");
+
+
+                    break;
+                case Operator.Circle:
+                    result = double.Pi * (numberInput1 * numberInput1);
+                    Console.WriteLine($"\nPi * ({numberInput1}*{numberInput1}) = {Math.Round(result, 2)}cm2 ist der Flächeninhalt des Kreises\n");
+                    result = 2 * numberInput1 * double.Pi;
+                    Console.WriteLine($"2 * {numberInput1} * Pi = {Math.Round(result, 2)}cm ist der Umfang des Kreises\n");
+                    break;
+                case Operator.Rectangle:
+                    result = numberInput1 * numberInput2;
+                    Console.WriteLine($"\n{numberInput1}*{numberInput2} = {Math.Round(result, 2)} Äpfel/Bananen ist der Flächeninhalt des Rechtecks\n");
+                    result = numberInput1 + numberInput2 + numberInput1 + numberInput2;
+                    Console.WriteLine($"\n{numberInput1} + {numberInput2} + {numberInput1} + {numberInput2}= {Math.Round(result, 2)} Äpfel/Bananen beträgt der Umfang des Rechtecks\n");
+                    break;
+            }
+            Console.ResetColor();
+            Console.WriteLine($"Du hast {Count} Berechnungen getätigt.");
+            Console.WriteLine("Drücke irgendwas für eine neue Berechnung");
+            Console.ReadKey();
+            return result;
+        }
+    }
+
+    public enum Operator
+    {
+        Add = 1,
+        Subtract = 2,
+        Multiply = 3,
+        Divide = 4,
+        Triangle = 5,
+        Circle = 6,
+        Rectangle = 7,
+    }
+}
---
+++ Aufgabe ENums/Program.cs
@@ -0,0 +1,72 @@
+﻿using Calculator_mit_ENums;
+
+double numberInput1 = 0;
+double numberInput2 = 0;
+double numberInputTriangle1 = 0;
+double numberInputTriangle2 = 0;
+double result = 0;
+
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("Was willst du rechnen?");
+        Console.WriteLine("\t1.Add\n\t2.Subtract\n\t3.Multiply\n\t4.Divide\n\t5.Triangle\n\t6.Circle\n\t7.Rectangle");
+        var menuInput = Console.ReadLine();
+    double menuInputIndex = double.Parse(menuInput);
+    Operator op = (Operator)menuInputIndex;
+    switch (op)
+    {
+        case Operator.Add:
+            numberInput1 = Calculator.GetNumberInput("Gebe die erste Zahl ein:\n");
+            numberInput2 = Calculator.GetNumberInput("Gebe die zweite Zahl ein::\n");
+            break;
+        case Operator.Subtract:
+            numberInput1 = Calculator.GetNumberInput("Gebe die erste Zahl ein:\n");
+            numberInput2 = Calculator.GetNumberInput("Gebe die zweite Zahl ein:\n");
+            break;
+        case Operator.Multiply:
+            numberInput1 = Calculator.GetNumberInput("Gebe die erste Zahl ein:\n");
+            numberInput2 = Calculator.GetNumberInput("Gebe die zweite Zahl ein:\n");
+            break;
+        case Operator.Divide:
+            numberInput1 = Calculator.GetNumberInput("Gebe die erste Zahl ein:\n");
+            numberInput2 = Calculator.GetNumberInput("Gebe die zweite Zahl ein:\n");
+            break;
+        case Operator.Triangle:
+            numberInput1 = Calculator.GetNumberInput("Gebe bitte die Grundseite des Dreiecks ein in cm:\n");
+            numberInput2 = Calculator.GetNumberInput("Gebe die Höhe des Dreiecks ein in cm:\n");
+            break;
+        case Operator.Circle:
+            numberInput1 = Calculator.GetNumberInput("Gebe den Radius des Kreises ein in cm:\n");
+            break;
+        case Operator.Rectangle:
+            numberInput1 = Calculator.GetNumberInput("Gebe die Lange Seite des Rechtecks an:\n");
+            numberInput2 = Calculator.GetNumberInput("Gebe die Breite Seite des Rechtecks an:\n");
+            break;
+    }
+
+    switch (op)
+    {
+        case Operator.Add:
+            Calculator.Calculate(numberInput1, numberInput2, Operator.Add);
+            break;
+        case Operator.Subtract:
+            Calculator.Calculate(numberInput1, numberInput2, Operator.Subtract);
+            break;
+        case Operator.Multiply:
+            Calculator.Calculate(numberInput1, numberInput2, Operator.Multiply);
+            break;
+        case Operator.Divide:
+            Calculator.Calculate(numberInput1, numberInput2, Operator.Divide);
+            break;
+        case Operator.Triangle:
+            Calculator.Calculate(numberInput1, numberInput2, Operator.Triangle);
+            break;
+        case Operator.Circle:
+            Calculator.Calculate(numberInput1, numberInput2, Operator.Circle);
+            break;
+        case Operator.Rectangle:
+            Calculator.Calculate(numberInput1, numberInput2, Operator.Rectangle);
+            break;
+    }
+    } while (true);
